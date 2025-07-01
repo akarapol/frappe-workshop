6e323de8d2efe2c808213e1b4bc79a7222f68519
Akarapol Kasivittayanun <akarapol@gmail.com>
2025-07-01 19:43:31 +0700

    doctype controller

--- workshop/training/doctype/customer/customer.py
+++ workshop/training/doctype/customer/customer.py
@@ -6,4 +6,15 @@ from frappe.model.document import Document
 
 
 class Customer(Document):
-	pass
+	def validate(self):
+		"""
+		Validates the Customer document before saving.
+		Checks if Date of Birth is provided when Identification Type
+		is "Personal Identification" and ensures it's not in the future.
+		"""
+		if self.identification_type == "Personal Identification":
+			if not self.date_of_birth:
+				frappe.throw("Date of Birth is required when Identification Type is Personal Identification.")
+			if self.date_of_birth and self.date_of_birth > frappe.utils.today():
+				frappe.throw("Date of Birth cannot be in the future.")
+
